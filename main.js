35fe7b8e6567d0310eaa2b66e536e7809d2bf989
AbhishekChoudhary23 <76167003+AbhishekChoudhary23@users.noreply.github.com>
2023-07-29 16:55:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let typed = new Typed("#text", {
+  strings: [
+    "Frontend Developer",
+    "National Basketball Player",
+    "Fingerstyle Guitarist",
+    "Web Developer",
+  ],
+  typeSpeed: 200,
+  backSpeed: 200,
+  backDelay: 300,
+  loop: true,
+});
